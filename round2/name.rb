11e82922d7f66665330c6ebd76c3a8d7eb0479e4
Michael B Lawrence <007mike@gmail.com>
2021-12-28 12:54:19 -0500

    Modified name.rb exercise

--- round2/name.rb
+++ round2/name.rb
@@ -1,3 +1,6 @@
-puts "What is your name?"
-name = gets.chomp
-puts "Hello, #{name}!"
+puts "What is your first name?"
+f_name = gets.chomp
+puts "What is your last name?"
+l_name = gets.chomp
+
+puts f_name + " " + l_name
